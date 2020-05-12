a008eceaba0ed52da53d3073d0f21f09a309924e
dbrannonhoward <dbrannonhoward@gmail.com>
2020-05-11 23:17:53 -0400

    addex example 08

--- example_06_css_etc.py
+++ example_06_css_etc.py
@@ -10,7 +10,7 @@ class StringGenerator(object):
     def index(self):
         return """<html>
           <head>
-            <link href="/static/css/style.css" rel="stylesheet">
+            <link href="/static/css/style_6.css" rel="stylesheet">
           </head>
           <body>
             <form method="get" action="generate">
